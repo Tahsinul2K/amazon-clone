f7869e5ecfeff22291c404f1e2421eb60ed159da
Tahsinul2K <tahsinuljubair@gmail.com>
2026-09-06 16:06:09 +0600

    moved folders for the frontend
    I accidentally made frontend in the backend. changed it to the root folder

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const app = express();
 // middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-app.use(express.static('frontend'));
+app.use(express.static('../frontend'));
 app.use(session({
     store: new  pgSession({
         pool,
